98031bcb19a69554fed5572e7baa2b31dc073ccd
Kazi Rahman <kazi.rahman@stud.th-deg.de>
2025-06-12 19:09:13 +0200

    Add real-time chicken detection; update video processing scripts; update .gitignore

--- single_test.py
+++ single_test.py
@@ -2,6 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO("runs/detect/train2/weights/best.pt")  
 results = model("tests/chick.jpg") 
-print(results)
+
 results[0].show()
-print(results[0].boxes)
