e5f14febacb37ede044a5840ff27ac4f11a18e37
Denisa-Iulia Vaidasigan <denisa-iulia.vaidasigan@stud.th-deg.de>
2025-06-20 18:56:18 +0200

    now counts both chickens and eggs in real time

--- real_time_detection.py
+++ real_time_detection.py
@@ -8,40 +8,24 @@ model = YOLO("runs/detect/train2/weights/best.pt")
 # --- Capture camera feed
 cap = cv2.VideoCapture(0)
 
-# --- Set the camera resolution to 640x480
-cap.set(3, 640)
-cap.set(4, 480)
+# --- Set the camera resolution to 1000x1000
+cap.set(3, 1000)
+cap.set(4, 1000)
 
 # --- Classnames the model can currently detect
-classNames = ["chicken"]
-# classNames = ["chicken", "egg"] # uncomment when the model is trained on eggs as well.
-
-chicken_count = 0
+classNames = ["chicken", "egg"]
 
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    # --- Visual parameters for .putText() 
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    fontScale = 1
-    color = (255, 0, 0)
-    thickness = 1
-    text = f"Chicken Count: {chicken_count}"
-    (text_width, text_height), _ = cv2.getTextSize(text, font, fontScale, thickness)
-    
-    # --- Have a 10 pixel buffer from the top right corner
-    x = img.shape[1] - text_width - 10 
-    y = text_height + 10 
-
-    # --- Display chicken count at the top right corner
-    cv2.putText(img, text, (x, y), font, fontScale, color, thickness)
-
     for r in results:
+        chicken_count = 0
+        egg_count = 0
         boxes = r.boxes
-        chicken_count = len(boxes)
 
         for box in boxes:
+
             # --- Detect bounding boxes
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
@@ -57,11 +41,40 @@ while True:
 
             # --- Only print out the bounding box and details about the object if we're at least 70% sure.
             if confidence > 0.70: 
-              # --- Display the rectangular bounding box
-              cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+              # --- Calculate chicken and egg count
+                if int(box.cls.item()) == 0:
+                    chicken_count += 1
+                else:
+                    egg_count += 1
 
-              # --- Display the text we want to show
-              cv2.putText(img, f"{classNames[cls]} {confidence}", org, font, fontScale, color, thickness)
+                # --- Display the rectangular bounding box
+                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+                # --- Display the text we want to show
+                cv2.putText(img, f"{classNames[cls]} {confidence}", org, font, fontScale, color, thickness)
+
+    # --- Visual parameters for .putText() 
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    color = (255, 0, 0)
+    thickness = 1
+    text_chicken = f"Chicken Count: {chicken_count}"
+    text_eggs = f"Egg Count: {egg_count}"
+    (text_width, text_height), _ = cv2.getTextSize(text_chicken, font, fontScale, thickness)
+    
+    # --- Have a 10 pixel buffer from the top right corner
+    x_chicken = img.shape[1] - text_width - 10 
+    y_chicken = text_height + 10 
+
+    # --- Placing the egg text
+    x_egg = img.shape[1] - text_width - 10
+    y_egg = text_height + 50
+
+    # --- Display chicken count at the top right corner
+    cv2.putText(img, text_chicken, (x_chicken, y_chicken), font, fontScale, color, thickness)
+
+    # --- Display egg counts at the top right corner
+    cv2.putText(img, text_eggs, (x_egg, y_egg), font, fontScale, color, thickness)
 
     cv2.imshow('Webcam', img)
 
--- train_model.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolo11n.pt")
-results = model.train(data="data.yaml", epochs=10, imgsz=640)
